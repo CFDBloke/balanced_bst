3ebba445d0a7f5ba70e759e59aad0dc5e0faf1f6
Tim Newton <manicmaniac_2000@yahoo.com>
2021-03-24 05:34:49 +0000

    Completed program with level, in, pre and post ordering functions, height and depth functions as a balance check and rebalance function

--- balanced_bst.rb
+++ balanced_bst.rb
@@ -25,7 +25,7 @@ module DeleteNodes
   def delete_two(node_to_delete)
     replacement_node = get_smallest(node_to_delete.right_child)
 
-    replacement_parent = parent(@root, replacement_node.data)
+    replacement_parent = parent(replacement_node.data)
 
     node_to_delete.data = replacement_node.data
 
@@ -34,10 +34,121 @@ module DeleteNodes
     else
       replacement_parent.left_child = replacement_node.right_child
     end
+  end
+end
 
-    # return unless node_to_delete.data == node_to_delete.right_child.data
+# Module managing the ordering of nodes
+module Ordering
+  def level_order
+    return if @root.nil?
 
-    # node_to_delete.right_child = node_to_delete.right_child.right_child
+    data = ''
+    queue = []
+    queue.push(@root)
+    print_level(queue, data)
+  end
+
+  def preorder
+    data = ''
+    print_preorder(@root, data)
+  end
+
+  def inorder
+    data = ''
+    print_inorder(@root, data)
+  end
+
+  def postorder
+    data = ''
+    print_postorder(@root, data)
+  end
+
+  private
+
+  def print_level(queue, data)
+    return data if queue.empty?
+
+    current = queue.first
+    data = "#{data}#{current.data} "
+    queue.push(current.left_child) unless current.left_child.nil?
+    queue.push(current.right_child) unless current.right_child.nil?
+    queue.shift
+
+    print_level(queue, data)
+  end
+
+  def print_preorder(node, data)
+    return data if node.nil?
+
+    data = "#{data}#{node.data} "
+    data = print_preorder(node.left_child, data)
+    print_preorder(node.right_child, data)
+  end
+
+  def print_inorder(node, data)
+    return data if node.nil?
+
+    data = print_inorder(node.left_child, data)
+    data = "#{data}#{node.data} "
+    print_inorder(node.right_child, data)
+  end
+
+  def print_postorder(node, data)
+    return data if node.nil?
+
+    data = print_postorder(node.left_child, data)
+    data = print_postorder(node.right_child, data)
+    "#{data}#{node.data} "
+  end
+end
+
+# Module that measures the metrics of the binary search tree
+module Metrics
+  def height(value)
+    node = find(value)
+    p find_height(node)
+  end
+
+  def depth(value)
+    node = find(value)
+    p find_depth(node)
+  end
+
+  def balanced?
+    check_balance(@root)
+  end
+
+  def rebalance
+    @array = inorder.split(' ').map(&:to_i)
+    @root = build_tree(@array, 0, @array.length - 1)
+  end
+
+  private
+
+  def find_height(node)
+    return -1 if node.nil?
+
+    left_height = find_height(node.left_child)
+    right_height = find_height(node.right_child)
+    [left_height, right_height].max + 1
+  end
+
+  def find_depth(node)
+    return 0 if node == @root
+
+    1 + find_depth(parent(node.data))
+  end
+
+  def check_balance(node)
+    return true if node.nil?
+
+    left_height = find_height(node.left_child)
+    right_height = find_height(node.right_child)
+
+    return false if (left_height - right_height).abs > 1
+
+    check_balance(node.left_child)
+    check_balance(node.right_child)
   end
 end
 
@@ -55,10 +166,11 @@ end
 # Tree class that builds the binary search tree made up of node objects
 class Tree
   include DeleteNodes
+  include Ordering
+  include Metrics
 
   def initialize(array)
     @array = array.sort.uniq
-    p @array
     @root = build_tree(@array, 0, @array.length - 1)
   end
 
@@ -75,7 +187,7 @@ class Tree
   end
 
   def insert(value)
-    leaf_node = find(@root, value)
+    leaf_node = find(value)
     if value < leaf_node.data
       leaf_node.left_child = Node.new(value)
     elsif value > leaf_node.data
@@ -86,8 +198,8 @@ class Tree
   end
 
   def delete(value)
-    node_to_delete = find(@root, value)
-    parent_node = parent(@root, value)
+    node_to_delete = find(value)
+    parent_node = parent(value)
     case no_of_children(node_to_delete)
     when 0
       delete_none(parent_node)
@@ -104,21 +216,21 @@ class Tree
     pretty_print(true, node.left_child, "#{prefix}#{is_left ? '    ' : '│   '}") if node.left_child
   end
 
-  private
-
-  def find(node, value)
+  def find(value, node = @root)
     if value < node.data
       return node if node.left_child.nil?
 
-      find(node.left_child, value)
+      find(value, node.left_child)
     elsif value > node.data
       return node if node.right_child.nil?
 
-      find(node.right_child, value)
+      find(value, node.right_child)
     else
       node
     end
   end
+
+  private
 
   def no_of_children(node)
     if node.left_child.nil? && node.right_child.nil?
@@ -130,15 +242,15 @@ class Tree
     end
   end
 
-  def parent(node, value)
+  def parent(value, node = @root)
     if value < node.data
       return node if node.left_child.data == value
 
-      parent(node.left_child, value)
+      parent(value, node.left_child)
     elsif value > node.data
       return node if node.right_child.data == value
 
-      parent(node.right_child, value)
+      parent(value, node.right_child)
     else
       node
     end
@@ -152,30 +264,47 @@ class Tree
   end
 end
 
-array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+def balance_script(bst)
+  puts "This binary search tree #{bst.balanced? ? 'IS ' : 'IS NOT '}balanced"
+end
+
+def order_script(bst)
+  puts " \n"
+  puts 'This is the array outputted using different ordering methods'
+  puts "Level: #{bst.level_order}"
+  puts "Pre: #{bst.preorder}"
+  puts "Post: #{bst.postorder}"
+  puts "In: #{bst.inorder}"
+  puts " \n"
+end
+
+array = Array.new(15) { rand(1..100) }
+
+puts 'This is the sample array of 15 numbers'
+p array
 
 bst = Tree.new(array)
 
-bst.insert(286)
-
-bst.insert(290)
-
-bst.delete(23)
-
-bst.delete(7)
-
+puts 'This is the binary search tree created from that array'
 bst.pretty_print(true)
 
-bst.delete(4)
+balance_script(bst)
+order_script(bst)
+
+array2 = Array.new(3) { rand(101..1000) }
+
+puts "This is the binary search tree after the randomly generated numbers, #{array2[0]}, #{array2[1]} and "\
+"#{array2[2]} have been inserted into it"
+
+array2.each { |num| bst.insert(num) }
 
 bst.pretty_print(true)
+balance_script(bst)
+puts " \n"
 
-bst.insert(50)
-
+puts 'This is the binary search tree after it has been rebalanced'
+bst.rebalance
 bst.pretty_print(true)
 
-bst.delete(8)
-
-bst.delete(9)
-
-bst.pretty_print(true)
+balance_script(bst)
+order_script(bst)
